805e093a6e78ffab90dafea4b9541386ce0c79b8
Tome94 <tommy_yang621@hotmail.com>
2023-03-13 23:42:33 +0000

    intial commit and fixes to React Routes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import HomePage from './pages/home';
+// eslint-disable-next-line
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+// eslint-disable-next-line
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/:type/:id">
+            <PetDetailsPage />
+          </Route>
+          <Route path="/:type?">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
